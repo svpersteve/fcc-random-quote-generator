fc0e515885b69ffdc092b92ecb34e0dd587727e3
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-16 12:00:43 +0100

    Add next quote route, controller action and link
    This adds a route so we can do a get request back to our quotes controller.
    We've directed this to the 'next' action in the controller, so Rails will run
    the code in that action.
    
    In this case it will fetch another random quote and render the index again
    using JavaScript only with the new variable we've assigned, which should be a
    new quote.
    
    This doesn't handle the case where the next quote chosen is the same as the
    first, which is a bug that some users will encounter.

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,4 +2,9 @@ class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
   end
+
+  def next
+    @quote = Quote.order("RANDOM()").first
+    render :index
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'quotes#index'
+  get '/next-quote', to: 'quotes#next', as: 'next_quote'
 end
