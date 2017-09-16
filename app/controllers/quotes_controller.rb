aa79da4b2df8b95f3c0e9a76f3678a09e8a85648
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-16 12:42:38 +0100

    Assign a random colour and use to style view

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -1,10 +1,18 @@
 class QuotesController < ApplicationController
   def index
     @quote = Quote.order("RANDOM()").first
+    @colour = colours.sample
   end
 
   def next
     @quote = Quote.order("RANDOM()").first
+    @colour = colours.sample
     render :index
+  end
+
+  private
+
+  def colours
+    ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"]
   end
 end
