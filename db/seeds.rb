603c7feed5dccefb8018d53f58a1a9fdb55c62a8
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-16 11:59:34 +0100

    Create seed data
    This creates some random quotes in the database when you run `rails db:seed`.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Quote.find_or_create_by(content: "I'm as mad as hell, and I'm not going to take this anymore!", author: 'Network')
+Quote.find_or_create_by(content: "Everybody pities the weak; jealousy you have to earn.", author: 'Arnold Schwarzenegger')
+Quote.find_or_create_by(content: "Whenever I climb I am followed by a dog called 'Ego'.", author: 'Friedrich Nietzsche')
+Quote.find_or_create_by(content: "You can't handle the truth!", author: 'A Few Good Men')
+Quote.find_or_create_by(content: "If you build it, he will come.", author: 'Field of Dreams')
+Quote.find_or_create_by(content: "Each problem that I solved became a rule which served afterwards to solve other problems.", author: 'Rene Descartes')
+Quote.find_or_create_by(content: "If a man does his best, what else is there?", author: 'General George S. Patton')
+Quote.find_or_create_by(content: "He who has a 'why' to live, can bear with almost any 'how'.", author: 'Friedrich Nietzsche')
+
+
